02715700f99d0388dbfce5817467a06d43b93d38
Joseph Ramirez Sarmiento <U20211C828@upc.edu.pe>
2023-04-15 10:25:06 -0500

    create R file

---
+++ upc-pre-202301-cc52-1-tp1.R
@@ -0,0 +1,17 @@
+#--instalando librerias
+
+install.packages("DescTools",dependencies = TRUE)
+install.packages("ggplot2",dependencies = TRUE)
+install.packages("agricolae",dependencies = TRUE)
+install.packages("mlr",dependencies = TRUE)
+
+library(mlr)
+library(agricolae)
+library(ggplot2)
+library(DescTools)
+
+#--cargando datos
+
+setwd("C:/Users/USUARIO/Desktop/DataScience-Grupo-1-Hotel-booking-demand")
+df<-read.table('hotel_bookings.csv',header=TRUE,sep=";",dec='.')
+head(df)
