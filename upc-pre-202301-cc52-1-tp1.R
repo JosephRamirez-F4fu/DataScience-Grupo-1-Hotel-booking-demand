9dc57946a1f3e238f0cce679c55530ed6a242502
Joseph Ramirez Sarmiento <U20211C828@upc.edu.pe>
2023-04-16 21:54:38 -0500

    change to factor and read table to read csv

--- upc-pre-202301-cc52-1-tp1.R
+++ upc-pre-202301-cc52-1-tp1.R
@@ -13,7 +13,7 @@ library(DescTools)
 #--cargando datos
 
 setwd("C:/Users/USUARIO/Desktop/DataScience-Grupo-1-Hotel-booking-demand")
-df<-read.table('hotel_bookings.csv',header=TRUE,sep=",",dec='.')
+df<-read.csv('hotel_bookings.csv',header=TRUE,sep=",")
 head(df)
 
 
@@ -22,33 +22,61 @@ df$hotel=as.factor(df$hotel)
 df$is_canceled=as.logical(df$is_canceled)
 df$lead_time=as.numeric(df$lead_time)
 df$arrival_date_week_number=as.integer(df$arrival_date_week_number)
-df$arrival_date_year=as.numeric(df$arrival_date_year)
+df$arrival_date_year=as.factor(df$arrival_date_year)
 df$arrival_date_month=as.factor(df$arrival_date_month)
 df$arrival_date_day_of_month=as.numeric(df$arrival_date_day_of_month)
-df$stays_in_weekend_nights=as.integer(df$adults)
-df$stays_in_week_nights=as.integer(df$stays_in_week_nights)
-df$adults=as.integer(df$adults)
-df$children=as.integer(df$children)
-df$babies=as.integer(df$babies)
+df$stays_in_weekend_nights=as.factor(df$adults)
+df$stays_in_week_nights=as.factor(df$stays_in_week_nights)
+df$adults=as.factor(df$adults)
+df$children=as.factor(df$children)
+df$babies=as.factor(df$babies)
 df$meal=as.factor(df$meal)
 df$country=as.factor(df$country)
 df$market_segment=as.factor(df$market_segment)
 df$distribution_channel=as.factor(df$distribution_channel)
-df$previous_cancellations=as.integer(df$previous_cancellations)
-df$previous_bookings_not_canceled=as.integer(df$previous_bookings_not_canceled)
+df$is_repeated_guest=as.factor(df$is_repeated_guest)
+df$previous_cancellations=as.factor(df$previous_cancellations)
+df$previous_bookings_not_canceled=as.factor(df$previous_bookings_not_canceled)
 df$reserved_room_type=as.factor(df$reserved_room_type)
 df$assigned_room_type=as.factor(df$assigned_room_type)
-df$booking_changes=as.integer(df$booking_changes)
+df$booking_changes=as.factor(df$booking_changes)
 df$deposit_type=as.factor(df$deposit_type)
 df$agent=as.factor(df$agent)
 df$company=as.factor(df$company)
 df$days_in_waiting_list=as.integer(df$days_in_waiting_list)
 df$customer_type=as.factor(df$customer_type)
 df$adr=is.numeric(df$adr)
-df$required_car_parking_spaces=as.numeric(df$required_car_parking_spaces)
-df$total_of_special_requests=as.numeric(df$total_of_special_requests)
+df$required_car_parking_spaces=as.factor(df$required_car_parking_spaces)
+df$total_of_special_requests=as.factor(df$total_of_special_requests)
 df$reservation_status=as.factor(df$reservation_status)
-df$reservation_status_date=as.Date(df$reservation_status_date)
+df$reservation_status_date=as.Date(df$reservation_status_date, format="%y-%m-%d")
 
-
+View(df)
+s<-table(df$market_segment)
+s<-table(df$company)
+s
 summary(df)
+
+names(df)
+str(df)
+
+sin_valor <- function(x){
+  sum = 0
+  for(i in 1:ncol(x))
+  {
+    cat("En la columna",colnames(x[i]),"total de valores NA:",colSums(is.na(x[i])),"\n")
+  }
+}
+
+sin_valor(df)
+
+en_blanco <- function(x){
+  sum = 0
+  for(i in 1:ncol(x))
+  {
+    cat("En la columna",colnames(x[i]),"total de valores en blanco:",colSums(x[i]==""),"\n")
+  }
+}
+en_blanco(df)
+
+
